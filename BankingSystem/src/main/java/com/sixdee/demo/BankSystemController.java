41ede291cba3d5bd29fa423bac37e77087762d06
2000SnehaBS <110822116+2000SnehaBS@users.noreply.github.com>
2022-09-06 18:39:06 +0530

    Update BankSystemController.java

--- BankingSystem/src/main/java/com/sixdee/demo/BankSystemController.java
+++ BankingSystem/src/main/java/com/sixdee/demo/BankSystemController.java
@@ -2,7 +2,12 @@ package com.sixdee.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,16 +15,21 @@ public class BankSystemController
 {
 	@Autowired
 	BankSystemRepo repo;
-	@RequestMapping("/")
-	public String home() 
-	{
-		return "home.jsp";
-	}
-	@RequestMapping("/addAccount")
-	public String addAccount(BankSystem account) 
+	
+	@PostMapping("/Account")
+	@ResponseBody
+	public BankSystem addAccount(@RequestBody BankSystem account) 
 	{
 		repo.save(account);
-		return "home.jsp";
+		return account;
+	}
+	
+	@DeleteMapping("/Account/{customerId}")
+	public String closeAccount(@PathVariable int customerId) {
+		BankSystem acc = repo.getOne(customerId);
+		repo.delete(acc);
+		return "account deleted";
+		
 	}
 	
 	
